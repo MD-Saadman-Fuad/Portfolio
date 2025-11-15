48cad8f16998e9287099a0b5917bf74fa47c8a68
MD-Saadman-Fuad <md.saadman.fuad@gmail.com>
2025-11-15 17:34:08 +0600

    Error Page handled

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,3 +1,19 @@
+import ErrorPage from '../assets/errorPage.png';
+import { Navbar } from '../components/Navbar';
+import { Footer } from '../components/Footer';
+import { Navigate } from 'react-router-dom';
 export const NotFound = () => {
-  return <div> NotFound</div>;
+
+  return (
+    <div >
+      <Navbar></Navbar>
+      <div className="flex flex-col items-center justify-center px-4">
+
+        <img className='mt-20 rounded-xl md:w-2/3' src={ErrorPage} alt="Error Page" />
+
+      </div>
+      <button onClick={() => Navigate("https://saadmanfuad.netlify.app/")} className="cosmic-button mt-20">Go Home</button>
+      <Footer></Footer>
+    </div>
+  );
 };
